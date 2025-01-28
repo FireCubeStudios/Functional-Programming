267fd5f31a69a11b37d0fd6553e037599f23c839
FireCubeStudios <40955345+FireCubeStudios@users.noreply.github.com>
2025-01-28 14:58:45 +0100

    Add project files.

---
+++ Functional/Program.fs
@@ -0,0 +1,20 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+
+// 1.1
+let sqr x : int = x * x;
+
+printfn "1.1: Square 4 = %d" (sqr(4))
+
+// 1.2
+let pow x n : float = System.Math.Pow(x, n)
+
+printfn "1.2: 4^2 = %d"
+// 1.3
+
+
+// more
+let rec fact = 
+    function
+    | 0 -> 1
+    | x when x > 0 -> x * fact(x - 1)
+    | _ -> failwith "";
