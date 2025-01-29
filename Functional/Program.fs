acd1e1d0d0e51348caef9ee7b45aebec02bbfbf4
FireCubeStudios <40955345+FireCubeStudios@users.noreply.github.com>
2025-01-30 00:00:17 +0100

    1.1 - 1.5

--- Functional/Program.fs
+++ Functional/Program.fs
@@ -1,20 +1,37 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
+﻿(*
+    Assignment 1 exercises
+    1.1 - 1.8 easy
+    1.9 - 1.10 medium
+    1.11 hard
+*)
 
 // 1.1
 let sqr x : int = x * x;
 
-printfn "1.1: Square 4 = %d" (sqr(4))
-
 // 1.2
 let pow x n : float = System.Math.Pow(x, n)
 
-printfn "1.2: 4^2 = %d"
 // 1.3
+let rec sum n : int = 
+    match n with
+    | 0 -> 0
+    | n when n > 0 -> n + sum(n - 1)
+    | _ -> failwith "Number was negative";;
 
+// 1.4
+let rec fib n: int =
+    match n with
+    | 0 -> 0
+    | 1 -> 1
+    | n when n > 1 -> fib(n - 1) + fib(n - 2)
+    | _ -> failwith "Number was negative";;
 
-// more
-let rec fact = 
-    function
-    | 0 -> 1
-    | x when x > 0 -> x * fact(x - 1)
-    | _ -> failwith "";
+// 1.5
+let dup s : string = s + s
+
+// 1.6
+let rec dupn s n = 
+    match n with
+    | 0 -> s
+    | n when n > 0 -> 
+    | _ -> failwith "n was negative"
